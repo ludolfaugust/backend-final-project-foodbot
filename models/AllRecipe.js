e27c146c697f00b0a3701464f89ff9c572ed23d8
ludo <85556848+ludolfaugust@users.noreply.github.com>
2021-11-16 14:07:57 +0100

    add new Schema

--- models/AllRecipe.js
+++ models/AllRecipe.js
@@ -8,30 +8,30 @@ const AllRecipeSchema = new Schema({
     required: [true, "Please add a recipeName"],
     maxlength: [99, "max 50 chars are allowed for the recipeName"],
   },
-  // image: {
-  //   type: String,
-  // },
+  image: {
+    type: String,
+  },
 
-  // ingredients: [
-  //   {
-  //     quantity: {
-  //       type: Number,
-  //       required: true,
-  //     },
-  //     ingredient: {
-  //       type: Schema.ObjectId,
-  //       ref: "Ingredient",
-  //     },
-  //   },
-  // ],
+  ingredients: [
+    {
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      ingredient: {
+        type: Schema.ObjectId,
+        ref: "Ingredient",
+      },
+    },
+  ],
 
-  // description: [
-  //   {
-  //     type: String,
-  //     required: [true, "Please add a description"],
-  //     maxlength: [5000, "max 50 chars are allowed for the description"],
-  //   },
-  // ],
+  description: [
+    {
+      type: String,
+      required: [true, "Please add a description"],
+      maxlength: [5000, "max 50 chars are allowed for the description"],
+    },
+  ],
 });
 
 module.exports = mongoose.model("AllRecipe", AllRecipeSchema);
